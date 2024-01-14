b0bde05d3950e831870173faeec0ba41dc219278
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-01-14 22:15:05 +0530

    update loader

--- src/app/movies/movies/movies.component.ts
+++ src/app/movies/movies/movies.component.ts
@@ -18,7 +18,6 @@ export class MoviesComponent implements OnInit {
   public added_to_watchlist: any;
   public is_added=false;
   public trailer_url= "https://www.youtube.com/embed/"
-  public loader=true;
 
   constructor(
     private router: ActivatedRoute,
@@ -30,7 +29,6 @@ export class MoviesComponent implements OnInit {
 
   ngOnInit() {
     this.router.paramMap.subscribe((params: any) => {
-      this.loader=false;
       let paramId = params?.params?.id;
       this.movieDetails(paramId);
       this.movieVideo(paramId);
